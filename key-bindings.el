6b9d40cf810c839d9a0e37ce98c12a50907bcedc
analysis918 <analysis918@gmail.com>
2012-10-25 15:59:18 +0800

    Adjust some key to emacs default & avoid using keys like C-S-letter.

--- key-bindings.el
+++ key-bindings.el
@@ -11,7 +11,7 @@
 (global-set-key (kbd "C-x M-f") 'find-or-create-file-at-point)
 (global-set-key (kbd "C-x C-j") 'open-buffer-directory)
 
-;; Ace jump mode
+;; Jumping
 (global-set-key (kbd "C-c SPC") 'ace-jump-mode)
 (global-set-key (kbd "M-z") 'zap-to-char)
 (global-set-key (kbd "M-Z") 'zap-to-char-exclusive)
@@ -19,10 +19,8 @@
 (global-set-key (kbd "M-M") 'jump-char-backward)
 
 ;; Mark multiple
-(global-set-key (kbd "C-<") 'mark-previous-like-this)
-(global-set-key (kbd "C->") 'mark-next-like-this)
-(global-set-key (kbd "C-M-m") 'mark-more-like-this)
-(global-set-key (kbd "C-M-S-m") 'mark-all-like-this)
+(global-set-key (kbd "C-,") 'mark-previous-like-this)
+(global-set-key (kbd "C-.") 'mark-next-like-this)
 (global-set-key (kbd "C-*") 'mark-all-like-this)
 
 ;; Expand region
@@ -34,28 +32,20 @@
 (define-key isearch-mode-map (kbd "C-w") 'upward-isearch-thing)
 
 ;; Edit shortcut
-;; (global-set-key "%" 'match-paren)
 (global-set-key (kbd "C-\"") 'toggle-quotes)
 (global-set-key (kbd "C-+") 'change-number-at-point)
 
+;; Kill 
 (global-set-key (kbd "C-x C-y") 'browse-kill-ring)
 
-;; (global-set-key (kbd "C-x p") 'etags-select-find-tag-at-point)
-(global-set-key (kbd "C-z") 'undo)
-(global-set-key (kbd "C-c a") 'go-to-char)
-
-;; Moving in line
+;; Skip empty or not when move
 (global-set-key (kbd "C-a") 'smart-beginning-of-line)
 (global-set-key (kbd "C-e") 'smart-end-of-line)
 
-;; Copying line
-(global-set-key (kbd "C-S-a") 'copy-to-beginning-of-line)
-(global-set-key (kbd "C-S-e") 'copy-to-end-of-line)
-
 ;; Killing line
-(global-set-key (kbd "C-S-K") 'kill-and-retry-line)
-(global-set-key (kbd "C-S-w") 'kill-region)
+(global-set-key (kbd "C-z") 'kill-region)
 (global-set-key (kbd "C-w") 'kill-region-or-backward-word)
+(global-set-key (kbd "C-x C-k") 'kill-and-retry-line)
 (global-set-key (kbd "C-c C-w") 'kill-to-beginning-of-line)
 
 ;; Change inner/outer
@@ -63,7 +53,7 @@
 (global-set-key (kbd "M-o") 'change-outer)
 
 ;; Duplicating
-(global-set-key (kbd "C-c C-d") 'duplicate-current-line-or-region)
+(global-set-key (kbd "C-c d") 'duplicate-current-line-or-region)
 
 ;; Creating new line
 (define-key global-map (kbd "RET") 'newline-and-indent)
@@ -83,15 +73,15 @@
 
 ;; Sometimes very useful
 (global-set-key (kbd "C-o") 'jump-to-space-forward)
-(global-set-key (kbd "C-S-o") 'mark-to-space-forward)
 
+;; Comment
 (global-set-key "\M-;" 'custom-comment-dwim)
 
 ;; Open link
 (global-set-key (kbd "C-c C-o") 'browse-url-at-point)
 
 ;; Indent
-(global-set-key [f7] 'indent-buffer)    
+(global-set-key [f7] 'indent-buffer)
 
 ;; Eval
 (global-set-key (kbd "C-c v") 'eval-buffer)
@@ -103,6 +93,7 @@
 (global-set-key (kbd "C-x C-m") 'smex)
 (global-set-key (kbd "C-c C-x C-m") 'smex-major-mode-commands)
 
+;; Menu
 (global-set-key (kbd "M-`") 'lacarte-execute-menu-command)
 
 (provide 'key-bindings)
